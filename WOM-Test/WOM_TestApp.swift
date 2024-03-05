1290ccaa3142385f1bc1b97b12c16de7c8521f6a
Felipe <felipe.ugarte.h@icloud.com>
2024-03-05 11:46:11 -0300

    Initial Commit

---
+++ WOM-Test/WOM_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WOM_TestApp.swift
+//  WOM-Test
+//
+//  Created by Felipe Ugarte on 05-03-24.
+//
+
+import SwiftUI
+
+@main
+struct WOM_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
