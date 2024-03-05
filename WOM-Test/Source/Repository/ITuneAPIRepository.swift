46b8bb03c91efb6f1669e17c7d87227391de0d69
Felipe <felipe.ugarte.h@icloud.com>
2024-03-05 13:37:08 -0300

    Create ITuneAPIRepository.swift

---
+++ WOM-Test/Source/Repository/ITuneAPIRepository.swift
@@ -0,0 +1,25 @@
+//
+//  iTunesAPIRepository.swift
+//  WOM-Test
+//
+//  Created by Felipe Ugarte on 05-03-24.
+//
+
+import Foundation
+
+class ITuneAPIRepository {
+    private let iTuneSongListRemoteDataSource: ITuneSongListRemoteDataSource
+
+    init(iTuneSongListRemoteDataSource: ITuneSongListRemoteDataSource = ITuneSongListRemoteDataSource()) {
+        self.iTuneSongListRemoteDataSource = iTuneSongListRemoteDataSource
+    }
+
+    func getItuneSongListRemoteDataSource() async throws -> [Track] {
+            do {
+                let tracks = try await iTuneSongListRemoteDataSource.fetchSongs()
+                return tracks
+            } catch {
+                throw error
+            }
+        }
+}
